f04446cc6402de4f464e9b5bfbdf2ddf3e6c8796
Mingfun Wong <mingfun.wong.chn@gmail.com>
2020-04-18 18:14:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,19 @@ const FIBOS = require('fibos.js');
 require('ssl').loadRootCerts();
 const config = require('./config');
 
-const client = FIBOS({
+const fibos = FIBOS({
   chainId: config['chain-id'],
   keyProvider: config['producer-priKey'],
   httpEndpoint: config['http-end-point'],
 });
 
 mian();
-setInterval(mian, 24 * 60 * 60 * 1000); // ever 1day do it
+setInterval(mian, 1 * 60 * 60 * 1000); // ever 1 hours do it
 
 async function mian() {
-  const eosioContract = await client.contract('eosio');
   try {
-    const ret = await eosioContract.claimbonus({
-      owner: config['producer-name']
-    }, {
-        authorization: config['producer-name']
-      });
-
-    console.log(ret);
+    const result = fibos.claimrewardsSync(config['producer-name'])
+    console.log(result);
   } catch (e) {
     console.error(e);
   }
